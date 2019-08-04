24cab97a9a74f691d691e876b516c2b1f0cecaac
vaclav_2012 <38167104+vaclav-2012@users.noreply.github.com>
2019-08-04 13:46:52 +0200

    Fix docstrings.

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
-"""@@@@@@Provide the Subreddit class."""
+"""Provide the Config class."""
 import logging
 import logging.config
 import os
@@ -9,12 +9,10 @@ import coloredlogs
 
 
 class Config:
-    """@@@@@@A class providing a set of functions to interact with the target
-    Subreddit.
-    """
+    """A class containing the configuration for the module."""
 
     def __init__(self, config_file=None):
-        """@@@@@@Construct an instance of the Subreddit object."""
+        """Construct an instance of the Config object."""
         config_list = [os.path.join(sys.path[0], "config_default.yml")]
 
         if config_file:
@@ -23,12 +21,16 @@ class Config:
         self.config = anyconfig.load(config_list)
 
     def __getitem__(self, key):
+        """Return an item of the config by key."""
         return self.config[key]
 
     def __contains__(self, key):
+        """Return true if an item exists in the config."""
         return key in config[key]
 
     def setup_logging(self):
+        """Setup the `logging` module using the configuration under the
+        `logging` key and setup the `coloredlogs` module."""
         logging.config.dictConfig(self.config["logging"])
 
         coloredlogs.install(
